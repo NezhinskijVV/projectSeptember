8865ee180f2b615cdf4709941a3389396ab847b7
NezhinskiyVV <SeekerOfConfessor@gmail.com>
2019-12-27 00:10:27 +0300

    chat client: add chat and registration

--- ChatClient/src/main/java/Main.java
+++ ChatClient/src/main/java/Main.java
@@ -2,6 +2,7 @@ import lombok.extern.java.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -13,17 +14,45 @@ public class Main {
     public static void main(String[] args) {
         try {
             Socket socket = null;
+            PrintWriter printWriter = null;
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
             while (true) {
                 if (socket == null) {
                     log.info("Start");
                     socket = new Socket(IP, PORT);
+                    printWriter = new PrintWriter(socket.getOutputStream());
+
+                    System.out.println("Введи логин: ");
+                    String login = bufferedReader.readLine();
+                    System.out.println("Введи пароль: ");
+                    String password = bufferedReader.readLine();
+
+                    printWriter.println("Registration login:" + login + "password:" + password);
+                    printWriter.flush();
+
+                    Socket finalSocket = socket;
+                    new Thread(() ->
+                    {
+                        try {
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(finalSocket.getInputStream()));
+                            String inputSocket;
+                            while ((inputSocket = reader.readLine()) != null) {
+                                System.out.println(inputSocket);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }).start();
+
                 } else {
-                    PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-                    printWriter.write("Registration");
+                    String message = bufferedReader.readLine();
+                    printWriter.println(message);
                     printWriter.flush();
                 }
             }
-        } catch (IOException e) {
+        } catch (
+                IOException e) {
             e.printStackTrace();
         }
     }
